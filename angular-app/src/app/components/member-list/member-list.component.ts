a19d1498db479d858fbe903f5d01cdbed72a5567
Joshua Kennedy <joshkennede@gmail.com>
2022-07-27 11:57:15 -0700

    added view all credit union members requirement

--- angular-app/src/app/components/member-list/member-list.component.ts
+++ angular-app/src/app/components/member-list/member-list.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { IMemberDto } from 'src/app/models/dto/member-dto.model';
+import { IMemberApiServiceProvider } from 'src/app/services/api/member/member-api.config';
+import { IMemberApiService } from 'src/app/services/api/member/member-api.service';
 
 @Component({
   selector: 'app-member-list',
@@ -6,10 +10,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+	
+	members: IMemberDto[] = [];
 
-  constructor() { }
+  constructor(
+		@Inject(IMemberApiServiceProvider)
+		private memberService: IMemberApiService
+	) { }
 
   ngOnInit(): void {
+		this.getMembers().subscribe((members: IMemberDto[]) => {
+			this.members = members;
+		});
   }
 
+	getMembers(): Observable<IMemberDto[]> {
+		return this.memberService.getCollection();
+	}
+
+	deleteMember(memberId: number): void {
+		this.memberService.delete(memberId);
+		this.getMembers();
+	}
 }
