1961d32931deac1d6fa97e489ac120b0814f63d7
Joshua Kennedy <joshkennede@gmail.com>
2022-07-27 11:21:57 -0700

    establish app routes

--- angular-app/src/app/app-routing.module.ts
+++ angular-app/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MemberCreateComponent } from './components/member-create/member-create.component';
+import { MemberInfoComponent } from './components/member-info/member-info.component';
+import { MemberListComponent } from './components/member-list/member-list.component';
 
-const routes: Routes = [];
+const routes: Routes = 
+[
+	{ path: '', component: MemberListComponent },
+	{ path: 'member/create', component: MemberCreateComponent },
+	{ path: 'member/:id', component: MemberInfoComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
