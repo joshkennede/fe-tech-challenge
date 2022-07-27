b4fc7ed3718555b149b8a1cd0fc8ae380e3f6af8
Joshua Kennedy <joshkennede@gmail.com>
2022-07-27 11:16:16 -0700

    adding base components per requirements

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MemberCreateComponent } from './components/member-create/member-create.component';
+import { MemberListComponent } from './components/member-list/member-list.component';
+import { MemberInfoComponent } from './components/member-info/member-info.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavBarComponent,
+    MemberCreateComponent,
+    MemberListComponent,
+    MemberInfoComponent,
   ],
   imports: [
     BrowserModule,
